9a4a895dab82e59fe8166fd20dcd4c63abdedc40
Rohith <60003721+rohith678@users.noreply.github.com>
2023-07-16 16:48:42 +0530

    Add files via upload

---
+++ RestapipracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapipracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapipracticeApplication.class, args);
+	}
+
+}
